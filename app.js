95fc484d9b706594bfa076c1a425b4b6511a76c8
bgomezg <borjagomezgomez@gmail.com>
2016-05-18 17:57:35 +0200

    - Improved dialogs. - Search pagination.

--- app.js
+++ app.js
@@ -4,49 +4,76 @@ var restify = require('restify');
 
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
-   token: 'xoxb-43644969201-GLi8yM1j0DWqX4Y2rT7Widfm'
+   token: ''
 });
+
+function search(session){
+  if (!session.userData.keyword) {
+        session.beginDialog('/keyword');
+  } else  if (!session.userData.fresh_search){
+      session.send('I don\'t understand your command: Use quit to finish the search');
+  }else{
+
+    var keyword = session.userData.keyword;
+    var page = session.userData.page;
+    var client = restify.createJsonClient({
+      url: 'https://api.github.com'
+    });
+
+
+    client.get('/search/code?q='+keyword+'+in:file+repo:azure/azure-content&page='+page+'&per_page=5', function(err, req, res, obj) {
+      if (obj.total_count>0){
+       session.send('I\'ve found these results for you:');
+        for( var k in obj.items){
+          var item = obj.items[k]
+          session.send(item.name+" ("+item.html_url+")" ) ;
+        }
+        session.send('Showing results from '+ (((page-1)*5)+1) +' to '+ page*5 + '. Number of results '+ obj.total_count + '. If you want to see more results please say \'more\'');
+        session.userData.fresh_search = false;
+
+      }else{
+        session.send('I haven\'t found any result. Please start your search again');
+        session.userData.fresh_search = true;
+        session.userData.page=1;
+        session.userData.keyword=null;
+        session.endDialog();
+      }
+      });
+
+
+
+    }
+}
 
 var slackBot = new builder.SlackBot(controller, bot);
 // Create bot and add dialogs
 slackBot.add('/', new builder.CommandDialog()
     .matches('^search', builder.DialogAction.beginDialog('/search'))
-    .matches('^quit', builder.DialogAction.endDialog())
+    .matches('^quit', function(session){
+      session.send('Thanks for using me! If you want to ')
+      builder.DialogAction.endDialog();
+  })
     .onDefault(function (session) {
           session.send('Hello! If you want to use me, say \'search\'');
     }));
 
 slackBot.add('/search', new builder.CommandDialog().matches('^quit', function (session){
-  session.send('Ending the search!')
   session.userData.keyword=null;
+  session.userData.page=null;
   session.endDialog();
 
 })
+  .matches('^more',function(session){
+        session.userData.page++;
+        session.userData.fresh_search=true;
+        search(session);
+  })
   .onDefault( function (session) {
-  if (!session.userData.keyword) {
-          session.beginDialog('/keyword');
-      } else  if (!session.userData.fresh_search){
-          session.send('I don\'t understand your command: Use quit to finish the search');
-      }else{
-
-  var keyword = session.userData.keyword;
-  var client = restify.createJsonClient({
-    url: 'https://api.github.com'
-  });
-
-
-  client.get('/search/code?q='+keyword+'+in:file+repo:azure/azure-content', function(err, req, res, obj) {
-     session.send('I\'ve found these results for you:');
-      for( var k in obj.items){
-        var item = obj.items[k]
-        session.send(item.name+" ("+item.html_url+")" ) ;
-      }
-
-  });
-  session.userData.fresh_search = false;
-
-}
+    search(session);
   }));
+
+
+
 
 
 slackBot.add('/keyword', [
@@ -56,6 +83,7 @@ slackBot.add('/keyword', [
     function (session, results) {
         session.userData.keyword = results.response;
         session.userData.fresh_search=true;
+        session.userData.page=1;
         session.endDialog();
     }
 ]);
